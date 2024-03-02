a20a9019ba34cf92920238aecbed0436763dd82c
K234k <kaitoueno2@gmail.com>
2024-03-02 23:44:02 +0900

    ホームビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
   get 'posts/index'
-  devise_for :users
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Defines the root path route ("/")
   # root "articles#index"
 end
