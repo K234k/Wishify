ccb6f64da90a7d562ef97be271dfbf94092a362e
K234k <kaitoueno2@gmail.com>
2024-03-16 22:37:52 +0900

    バリテーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :password,
-            format: { with: PASSWORD_REGEX, message: 'must include both letters and numbers' }
-         validates :name, presence: true
+       devise :database_authenticatable, :registerable,
+              :recoverable, :rememberable, :validatable
+     
+       PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+       validates :password,
+                 format: { with: PASSWORD_REGEX, message: 'must include both letters and numbers' }
+       validates :nickname, presence: true
+       validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－一-龠々]+\z/ }
+       validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー－一-龠々]+\z/ }
+       validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+       validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+       validates :birthday, presence: true
 end
