5256427a5424e4b3c422d0d5c53135176b790559
K234k <kaitoueno2@gmail.com>
2024-03-16 23:40:34 +0900

    バリテーション修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_08_143916) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_16_141108) do
   create_table "users", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
