e63a1d2cf538acf67c8ed1d5a908d8964af3104d
K234k <kaitoueno2@gmail.com>
2024-03-18 23:15:56 +0900

    テストコード記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,45 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # 有効なユーザーのテスト
-  it "is valid with a name, email, and password" do
-    user = FactoryBot.build(:user)
-    expect(user).to be_valid
+  before do
+    @user = FactoryBot.build(:user)
   end
 
-  # 名前がない場合は無効なユーザーのテスト
-  it "is invalid without a name" do
-    user = FactoryBot.build(:user, name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
-  end
-
-  # メールアドレスがない場合は無効なユーザーのテスト
-  it "is invalid without an email" do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-
-  # パスワードがない場合は無効なユーザーのテスト
-  it "is invalid without a password" do
-    user = FactoryBot.build(:user, password: nil)
-    user.valid?
-    expect(user.errors[:password]).to include("can't be blank")
-  end
-
-  # 重複したメールアドレスの場合は無効なユーザーのテスト
-  it "is invalid with a duplicate email address" do
-    FactoryBot.create(:user, email: "test@example.com")
-    user = FactoryBot.build(:user, email: "test@example.com")
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
-  end
-
-  # パスワードが6文字未満の場合は無効なユーザーのテスト
-  it "is invalid with a password less than 6 characters" do
-    user = FactoryBot.build(:user, password: "short", password_confirmation: "short")
-    user.valid?
-    expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
-  end
-end
+  describe 'ユーザー新規登録' do
+    context 'ユーザー登録ができる場合' do
+      it '情報が正しく入力されていれば新規登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'パスワードが６文字以上半角英数字であれば登録できる' do
+        @user.password = 'test123'
+        @user.password_confirmation = 'test123'
+        expect(@user).to be_valid
+      end
+      it '名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が出来れば登録できる' do
+        @user.last_name = '山田'
+        expect(@user).to be_valid
+      end
+      it '名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が出来れば登録できる' do
+        @user.first_name = '太郎'
+        expect(@user).to be_valid
+      end
+      it '名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が出来れば登録できる' do
+        @user.last_name_kana = 'ヤマダ'
+        expect(@user).to be_valid
+      end
+      it '名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が出来れば登録できる' do
+        @user.first_name_kana = 'タロウ'
+        expect(@user).to be_valid
+      end
+    end
