b1762d7eaa7d2d93624b49b581e9af8a42f49173
K234k <kaitoueno2@gmail.com>
2024-03-18 22:47:23 +0900

    ユーザーモデルファクトリーの定義

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name { "テストユーザー" }
-    email { "test@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
+    name             { Faker::Name.name }
+    email            { Faker::Internet.email }
+    password         { "1a#{Faker::Internet.password(min_length: 6)}" }
+    password_confirmation { password }
+    last_name        { '山田' }
+    first_name       { '太郎' }
+    last_name_kana   { 'ヤマダ' }
+    first_name_kana  { 'タロウ' }
+    birthday         { Faker::Date.birthday }
   end
 end
